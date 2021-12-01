c926a13ca83ccd1ed8909140003f8e6fe1a167d5
Nguyễn Thu Thảo Châu <thaochau2201@gmail.com>
2021-12-01 16:51:05 +0700

    Update Admin menu

--- Nanocovax/src/nanocovax/AdminMenu.java
+++ Nanocovax/src/nanocovax/AdminMenu.java
@@ -1,65 +1,102 @@
 package nanocovax;
 
 import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
+import javax.swing.table.DefaultTableModel;
+import java.awt.event.*;
 
-public class AdminMenu {
+public class AdminMenu extends JFrame {
     private JPanel rootPanel;
-    private JLabel ndtLabel;
-    private JLabel nqlLabel;
-    private JPanel Panel1;
-    private JLabel logoutLabel;
-    private JButton addBtn;
+    private JButton addButton;
     private JTextField textField1;
-    private JButton searchBtn;
-    private JTable jTable;
-    private JScrollPane scrollPanel;
-    private JButton hisBtn;
-    private JButton removeBtn;
+    private JButton searchButton;
+    private JLabel lbNQL;
+    private JLabel lbNDT;
+    private JLabel lbLogout;
+    private JLabel header;
+    private JButton historyButton;
+    private JButton removeButton;
+    private JTable table;
+    private JButton editButton;
+    private JButton refreshButton;
 
-
-    public AdminMenu() {
-
-
-        nqlLabel.addMouseListener(new MouseAdapter() {
-            @Override
-            public void mouseClicked(MouseEvent e) {
-                super.mouseClicked(e);
-            }
-        });
-        ndtLabel.addMouseListener(new MouseAdapter() {
-            @Override
-            public void mouseClicked(MouseEvent e) {
-                super.mouseClicked(e);
-            }
-        });
-        logoutLabel.addMouseListener(new MouseAdapter() {
-            @Override
-            public void mouseClicked(MouseEvent e) {
-                super.mouseClicked(e);
-            }
-        });
-        addBtn.addActionListener(new ActionListener() {
+    AdminMenu(){
+        add(rootPanel);
+        createTable();
+        setSize(1200,600);
+        //setResizable(false);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+        addButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
 
             }
         });
-        searchBtn.addActionListener(new ActionListener() {
+        searchButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
 
+            }
+        });
+
+        historyButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        removeButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        editButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+
+        refreshButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                //làm mới lại table
+            }
+        });
+        lbNQL.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                super.mouseClicked(e);
+
+            }
+        });
+        lbNDT.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                super.mouseClicked(e);
+                //chuyển sang màn hình quản lí nơi điều trị
+            }
+        });
+        lbLogout.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                super.mouseClicked(e);
             }
         });
     }
+    public JPanel getRootPanel(){
+        return this.rootPanel;
+    }
+
+
+    public void createTable(){
+        String[] tbColName = {"ID", "Phân quyền", "Tình trạng"};
+        Object[] [] data = {{"01","Admin","bt"}};
+        table.setModel(new DefaultTableModel(data,tbColName));
+        
+    }
     public static void main(String[] args){
-        JFrame frame = new JFrame("Admin");
-        frame.setContentPane(new AdminMenu().rootPanel);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(800,600);
-        frame.setVisible(true);
+        AdminMenu adminMenu = new AdminMenu();
     }
 }
